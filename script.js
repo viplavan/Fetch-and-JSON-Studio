d4da32490923edc0a9a4875b2b76aed997211924
github viplavan <viplava.kilaru@gmail.com>
2020-12-07 19:47:26 -0600

    Added Bio in the HTML Dynamically and fetching the details from API in Script file

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 // TODO: add code here
+window.addEventListener("load", function () {
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function (response) {
+        response.json().then(function (json) {
+            console.log(json);
+            let bio = document.querySelector(".bio");
+            bio.innerText = astronaut[0].
+        });
+    });
+});
